aea00aa1a3fe291c545e8643fab52f6af1277fd5
Peter Arnold <peter@deepsixdesign.com>
2019-12-07 12:51:50 -0500

    asd

--- index.js
+++ index.js
@@ -1,46 +1,12 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const vision = require('@google-cloud/vision');
-const visionClient =  new vision.ImageAnnotatorClient();
-let Promise = require('promise');
-admin.initializeApp(functions.config().firebase);
-const db = admin.firestore();
+// Copyright 2019 Google LLC. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
 
+// [START run_imageproc_server]
+const app = require('./app.js');
+const PORT = process.env.PORT || 8080;
 
-exports.addSimilarImages = functions.firestore.document('photos/{document}')
-.onCreate((snap, context) => {
-
-	console.log('SNAP', snap)
-	console.log('CONTEXT', context)
-
-	const data = snap.data();
-	console.log('DATA IN IS', data)
-	const photoUrl = "gs://" + data.bucket + "/" + data.fullPath;
-	console.log('url is', photoUrl);
-
-	return Promise.resolve()
-	.then(() => {
-		return visionClient.webDetection(photoUrl);
-	})
-	.then(results => {
-		console.log('VISION data all is: ', results)
-		const webDetection = results[0].webDetection
-
-		let similarImages = [];
- 		if (webDetection.visuallySimilarImages.length) {
-			webDetection.visuallySimilarImages.forEach(image => {
-			 	similarImages.push(image);
-			 });
-		}		
-
-		console.log('similarImages', similarImages)
-
-		db.collection('photos').doc(context.params.document).update({ similarImages })
-		.then(res => console.log('dopples added'))
-		.catch(err => console.error(err));
-
-
-	})
-	.catch(err => console.error(err));
-
-})
+app.listen(PORT, () =>
+  console.log(`nodejs-image-processing listening on port ${PORT}`)
+);
+// [END run_imageproc_server]
