cbd6563360b44683aa5e15f0013674574868f457
Peter Arnold <peter@deepsixdesign.com>
2020-01-31 11:27:05 -0500

    asd

--- app.js
+++ app.js
@@ -4,23 +4,23 @@
 
 // [START run_imageproc_controller]
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
 
-const image = require('./image');
+const image = require("./image");
 
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
   if (!req.body) {
-    const msg = 'no Pub/Sub message received';
+    const msg = "no Pub/Sub message received";
     console.error(`error: ${msg}`);
     res.status(400).send(`Bad Request: ${msg}`);
     return;
   }
   if (!req.body.message || !req.body.message.data) {
-    const msg = 'invalid Pub/Sub message format';
+    const msg = "invalid Pub/Sub message format";
     console.error(`error: ${msg}`);
     res.status(400).send(`Bad Request: ${msg}`);
     return;
@@ -30,14 +30,15 @@ app.post('/', async (req, res) => {
   const pubSubMessage = req.body.message;
   let data;
   try {
-    data = Buffer.from(pubSubMessage.data, 'base64')
+    data = Buffer.from(pubSubMessage.data, "base64")
       .toString()
       .trim();
     data = JSON.parse(data);
   } catch (err) {
     const msg =
-      'Invalid Pub/Sub message: data property is not valid base64 encoded JSON';
+      "Invalid Pub/Sub message: data property is not valid base64 encoded JSON";
     console.error(`error: ${msg}: ${err}`);
+    console.error(`req.body.message: ${req.body.message}`);
     res.status(400).send(`Bad Request: ${msg}`);
     return;
   }
@@ -45,7 +46,7 @@ app.post('/', async (req, res) => {
   // Validate the message is a Cloud Storage event.
   if (!data.name || !data.bucket) {
     const msg =
-      'invalid Cloud Storage notification: expected name and bucket properties';
+      "invalid Cloud Storage notification: expected name and bucket properties";
     console.error(`error: ${msg}`);
     res.status(400).send(`Bad Request: ${msg}`);
     return;
