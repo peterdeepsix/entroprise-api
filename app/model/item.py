e1ce16b5e03c5031f4329f538a6f2bcdc7d523c7
Peter Arnold <peter@deepsixdesign.com>
2020-06-16 10:26:26 -0400

    asd

--- app/model/item.py
+++ app/model/item.py
@@ -5,22 +5,31 @@ from model.doc_model_base import DocModelBase
 
 
 class ItemBaseInDB(DocModelBase):
-    __table_name__ = "items"
+    __table_name__ = "root/items/items"
 
 
 class Item(ItemBaseInDB):
     item_id: str
     name: str
-    create_time: datetime
-    update_time: datetime = None
+    content: str
+    question: str
+    confidence: int
+    createdate: datetime
+    lastupdate: datetime = None
 
 
 class ItemCreate(ItemBaseInDB):
     item_id: str
     name: str
-    create_time: datetime = datetime.utcnow()
+    content: str
+    question: str
+    confidence: int
+    createdate: datetime = datetime.utcnow()
 
 
 class ItemUpdate(ItemBaseInDB):
     name: str
-    update_time: datetime = datetime.utcnow()
+    content: str
+    question: str
+    confidence: int
+    lastupdate: datetime = datetime.utcnow()
